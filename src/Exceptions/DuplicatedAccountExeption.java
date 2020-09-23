38437e46bd9ca5e6b68eb01defb8b003e420bb85
Maxim Shatrov <maximshatrov163@gmail.com>
2020-09-23 15:44:34 +0400

    package Exeptions -> package Exceptions

--- src/Exceptions/DuplicatedAccountExeption.java
+++ src/Exceptions/DuplicatedAccountExeption.java
@@ -1,4 +1,4 @@
-package Exeptions;
+package Exceptions;
 
 public class DuplicatedAccountExeption extends CardExeption {
     public DuplicatedAccountExeption(){
--- src/Exceptions/WrongPincodeExeption.java
+++ src/Exceptions/WrongPincodeExeption.java
@@ -1,4 +1,4 @@
-package Exeptions;
+package Exceptions;
 
 public class WrongPincodeExeption extends CardExeption {
     public WrongPincodeExeption(int attempt) {
